5403410a371d5450a3716c288c54661251b6976d
James Barber <james@jamesbarber.tech>
2020-04-29 17:19:15 +0100

    Created static frontend

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title>Jamescape Store</title>
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,700|Comfortaa:300,400|Roboto+Condensed:700,700i" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/store.css" />
+	</head>
+	<body>
+		<section class="header">
+			<div class="container">
+				<h1 class="display-4"><span class="regular">Jamescape</span> Store</h1>
+			</div>
+		</section>
+		<section class="feature">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h2 class="white">NHS Teddy Bears</h2>
+						<p class="lead">100% of profits from our latest merchandise line go directly to support the NHS.</p>
+						<p>These stylish teddy bears adorning the Jamescape TV logo across their t-shirts and an adorable "Supporting our NHS" ribbon are a great way to remember these challenging times for years to come while supporting those who are leading the battle against the virus.</p>
+						<p>This limited edition merchandise line is sold directly by Jamescape and is available to buy individually or in a family pack of 5!</p>
+						<h2 class="white">£4.00 <a href="" class="btn btn-light btn-lg">Buy now</a></h2>
+					</div>
+					<div class="col-md-4">
+						<h4>All pre-orders come with a signed Jamescape TV postcard!</h4>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="products">
+			<div class="container">
+				<h2>What's Hot</h2>
+				<p>We're still selling all of our other merchandise items as normal through the our distribution partners, TShirt Studio! See what we've got on offer below...</p>
+				<div class="row">
+					
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
